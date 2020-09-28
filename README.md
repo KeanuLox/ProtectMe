e461ab35cc436c0b234db800eb1cef460c180aa3
Keanu <28869072+KeanuLox@users.noreply.github.com>
2020-09-28 13:16:39 +0000

    ip

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
 # ProtectMe
 safe your code from dumb people. <br />
-
-## How it works
-you have your code on your own server, where a webserver is hosted. with a apiKey and the sourceName can your client load the .js file (nodeJS only).
-
-## create executable
-first you need to install the pkg package on your pc <br />
-`(sudo) npm i -g pkg` <br />
-then use it on the client.js <br />
-`pkg --debug client.js` <br />
--- server.js
+++ server.js
@@ -10,14 +10,18 @@ var masterKey = "1337a1337";
 //apiKey Map
 var apiKeys = new Map();
 
+var apiKeyIPs = new Map();
+
 setInterval(() => {
     fs.readFileSync('./apiKeys.txt').toString().split('\n').forEach(_raw => {
         var _apiKey = _raw.split(',')[0];
         var _source = _raw.split(',')[1];
+        var _ip = _raw.split(',')[2];
     
         if(apiKeys.get(_source) == undefined) {
             console.log('new apiKey found: ' + _apiKey);
             apiKeys.set(_source,_apiKey);
+            apiKeyIPs.set(_apiKey,_ip);
             console.log('new apiKey registered: ' + _apiKey);
         }
     });
@@ -29,12 +33,13 @@ app.get('/createAPIKey',(req,res) => {
     var _masterKey = new Buffer(url.parse(req.url,true).query.mkey);
     var _apiKey = new Buffer(url.parse(req.url,true).query.api,'base64').toString('ascii');
     var _source = url.parse(req.url,true).query.source;
+    var _ip = url.parse(req.url,true).query.ip;
 
     //ask if queries exists
-    if(_apiKey != undefined && _source != undefined) {
+    if(_apiKey != undefined && _source != undefined && _ip != undefined) {
         //ask if the original masterkey and the sended one are the same
         if(_masterKey == masterKey) {
-            fs.writeFileSync('./apiKeys.txt',`${fs.readFileSync('./apiKeys.txt')}\n_${_apiKey},${_source}`);
+            fs.writeFileSync('./apiKeys.txt',`${fs.readFileSync('./apiKeys.txt')}\n_${_apiKey},${_source},${_ip}`);
             apiKeys.set(_source,_apiKey);
             res.send('apiKey created.');
         } else {
@@ -55,8 +60,12 @@ app.get('/getSource',(req,res) => {
         //ask if the original apiKey and the sended one are the same
         var key = apiKeys.get(_source);
         if(key == _apiKey) {
-            //code goes brr
-            res.send(fs.readFileSync(`./source/${_source}.js`));
+            if(req.ip == apiKeyIPs.get(_apiKey)) {
+                //code goes brr
+                res.send(fs.readFileSync(`./source/${_source}.js`));
+            } else {
+                res.send('console.log("ip blocked");');
+            }
         } else if(key == undefined) {
             //error message
             res.send('console.log("source not found.")');
